c891615977e59655a598e5d45e987da91d71bff8
Wojciech <cichockiwojciech97@gmail.com>
2023-11-17 17:49:14 +0100

    Use decimal type in contract

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+use decimal::Decimal;
+use odra::{types::U256, Variable};
+use types::liquidity::Liquidity;
+use uints::U256T;
+
 extern crate alloc;
 
 pub mod consts;
@@ -7,13 +12,17 @@ pub mod uints;
 
 #[odra::module]
 pub struct Invariant {
-
+    liquidity: Variable<U256>,
 }
 
 #[odra::module]
 impl Invariant {
     #[odra(init)]
-    pub fn init(&mut self) {}
+    pub fn init(&mut self) {
+        let liquidity = Liquidity::new(U256T::from(100_000_000u128));
+        let liquidity_u256 = U256::from_dec_str(liquidity.get().to_string().as_str()).unwrap();
+        self.liquidity.set(liquidity_u256);
+    }
 }
 
 #[cfg(test)]
