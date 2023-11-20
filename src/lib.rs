358ac4f6755c22532e65e750717c9a7a7b02620b
Wojciech <cichockiwojciech97@gmail.com>
2023-11-21 00:30:49 +0100

    Init state inside Invariant contract

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use alloc::string::ToString;
 use decimal::Decimal;
 use invariant_math::liquidity::Liquidity;
 use invariant_math::uints::U256T;
-use odra::{types::U256, Variable};
+use odra::{types::{U256, U128}, Variable, contract_env};
 
 #[odra::module]
 pub struct Invariant {
@@ -21,9 +21,14 @@ pub struct Invariant {
 impl Invariant {
     #[odra(init)]
     pub fn init(&mut self) {
+        let caller = contract_env::caller();
         let liquidity = Liquidity::new(U256T::from(100_000_000u128));
         let liquidity_u256 = U256::from_dec_str(liquidity.get().to_string().as_str()).unwrap();
         self.liquidity.set(liquidity_u256);
+        self.state.set(State {
+            admin: caller,
+            protocol_fee: U128::from(10000000000u128),
+        })
     }
 }
 
