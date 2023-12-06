9da003967c16c74d8f53ecacc159d7883ba1e5bb
Sniezka <dygons14@gmail.com>
2023-12-06 15:07:21 +0100

    Configured positions collection

---
+++ src/contracts/errors.rs
@@ -0,0 +1,11 @@
+use odra::execution_error;
+
+execution_error! {
+    pub enum InvariantExecutionError {
+        TickAlreadyExist => 1,
+        TickNotFound => 2,
+        PoolAlreadyExist => 3,
+        PoolNotFound => 4,
+        PositionNotFound => 5,
+    }
+}
