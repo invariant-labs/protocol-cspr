b4c655329c2d014fe42ae9ba692cf054985c1db1
Sniezka <dygons14@gmail.com>
2023-12-11 12:50:48 +0100

    Added change protocol fee tests

---
+++ src/e2e/change_protocol_fee.rs
@@ -0,0 +1,38 @@
+use crate::math::percentage::Percentage;
+use crate::InvariantDeployer;
+use crate::InvariantError;
+use decimal::*;
+use odra::test_env;
+use odra::types::U128;
+
+#[test]
+fn test_change_protocol_fee() {
+    let deployer = test_env::get_account(0);
+    test_env::set_caller(deployer);
+    let mut invariant = InvariantDeployer::init(Percentage::new(U128::from(0)));
+
+    let protocol_fee = invariant.get_protocol_fee();
+    assert_eq!(protocol_fee, Percentage::new(U128::from(0)));
+
+    let new_fee = Percentage::new(U128::from(1));
+    invariant.change_protocol_fee(new_fee).unwrap();
+
+    let protocol_fee = invariant.get_protocol_fee();
+    assert_eq!(protocol_fee, new_fee);
+}
+
+#[test]
+fn test_change_protocol_fee_not_adming() {
+    let deployer = test_env::get_account(0);
+    test_env::set_caller(deployer);
+    let mut invariant = InvariantDeployer::init(Percentage::new(U128::from(0)));
+
+    let protocol_fee = invariant.get_protocol_fee();
+    assert_eq!(protocol_fee, Percentage::new(U128::from(0)));
+
+    let new_fee = Percentage::new(U128::from(1));
+    test_env::set_caller(test_env::get_account(1));
+    let result = invariant.change_protocol_fee(new_fee);
+
+    assert_eq!(result, Err(InvariantError::NotAdmin));
+}
--- src/e2e/mod.rs
+++ src/e2e/mod.rs
@@ -1,5 +1,6 @@
 pub mod add_fee_tier;
 pub mod change_fee_receiver;
+pub mod change_protocol_fee;
 pub mod constructor;
 pub mod create_pool;
 pub mod remove_fee_tier;
