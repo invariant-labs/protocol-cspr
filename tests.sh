f844631d29f2e09949f038d78a96784f8a37cc5f
zielvna <zielvna@gmail.com>
2023-12-12 10:27:43 +0100

    add token contract to odra toml

--- tests.sh
+++ tests.sh
@@ -8,13 +8,6 @@ cd decimal_core
 cargo test
 cd ../../..
 
-cd src/token
-cargo fmt --all -- --check
-cargo clippy --all-targets -- --no-deps -D warnings
-cargo odra test
-cargo odra build -b casper
-cd ../..
-
 cargo fmt --all -- --check
 cargo clippy --all-targets -- --no-deps -D warnings
 
