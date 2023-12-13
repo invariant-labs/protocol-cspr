0d5c3e50d19f4e03b603dfa9edf14e622194cde8
Sniezka <dygons14@gmail.com>
2023-12-13 10:32:14 +0100

    Added cross tests

--- src/e2e/mod.rs
+++ src/e2e/mod.rs
@@ -3,6 +3,7 @@ pub mod change_fee_receiver;
 pub mod change_protocol_fee;
 pub mod constructor;
 pub mod create_pool;
+pub mod cross;
 pub mod position;
 pub mod protocol_fee;
 pub mod remove_fee_tier;
