62e9967037ad03a798b0a8e277ea3bb61b4336ce
zielvna <zielvna@gmail.com>
2023-12-14 12:08:05 +0100

    add deposit limits at upper limit test to limits tests

--- src/e2e/limits.rs
+++ src/e2e/limits.rs
@@ -1,8 +1,11 @@
 use crate::contracts::{get_liquidity_by_x, get_liquidity_by_y, FeeTier, PoolKey};
 use crate::math::get_delta_y;
 use crate::math::sqrt_price::calculate_sqrt_price;
+use crate::math::sqrt_price::get_max_tick;
 use crate::math::token_amount::TokenAmount;
-use crate::math::{percentage::Percentage, sqrt_price::SqrtPrice, MAX_SQRT_PRICE, MIN_SQRT_PRICE};
+use crate::math::{
+    percentage::Percentage, sqrt_price::SqrtPrice, MAX_SQRT_PRICE, MAX_TICK, MIN_SQRT_PRICE,
+};
 use crate::token::{TokenDeployer, TokenRef};
 use crate::{InvariantDeployer, InvariantRef};
 use decimal::{Decimal, Factories};
@@ -262,4 +265,60 @@ fn test_limits_big_deposit_both_tokens() {
     let contract_amount_y = token_y.balance_of(invariant.address());
     assert_eq!(contract_amount_x, U256::from_dec_str(mint_amount).unwrap());
     assert_eq!(contract_amount_y, y.get());
+}
+
+#[test]
+fn test_deposit_limits_at_upper_limit() {
+    let (mut invariant, mut token_x, mut token_y) = init_dex_and_tokens_max_mint_amount();
+
+    let mint_amount = "110427941548649020598956093796432407239217743554726184882600387580788736"; // 2^236
+    token_x.approve(invariant.address(), &U256::max_value());
+    token_y.approve(invariant.address(), &U256::max_value());
+
+    let fee_tier = FeeTier::new(Percentage::from_scale(6, 3), 1).unwrap();
+    invariant.add_fee_tier(fee_tier).unwrap();
+
+    let init_tick = get_max_tick(1);
+    let init_sqrt_price = calculate_sqrt_price(init_tick).unwrap();
+    invariant
+        .create_pool(
+            *token_x.address(),
+            *token_y.address(),
+            fee_tier,
+            init_sqrt_price,
+            init_tick,
+        )
+        .unwrap();
+
+    let pool = invariant
+        .get_pool(*token_x.address(), *token_y.address(), fee_tier)
+        .unwrap();
+    assert_eq!(pool.current_tick_index, init_tick);
+    assert_eq!(pool.sqrt_price, calculate_sqrt_price(init_tick).unwrap());
+
+    let position_amount = U256::from_dec_str(mint_amount).unwrap() - U256::from(1);
+
+    let liquidity_delta = get_liquidity_by_y(
+        TokenAmount::new(position_amount),
+        0,
+        MAX_TICK,
+        pool.sqrt_price,
+        false,
+    )
+    .unwrap()
+    .l;
+
+    let pool_key = PoolKey::new(*token_x.address(), *token_y.address(), fee_tier).unwrap();
+    let slippage_limit_lower = pool.sqrt_price;
+    let slippage_limit_upper = pool.sqrt_price;
+    invariant
+        .create_position(
+            pool_key,
+            0,
+            MAX_TICK,
+            liquidity_delta,
+            slippage_limit_lower,
+            slippage_limit_upper,
+        )
+        .unwrap();
 }
