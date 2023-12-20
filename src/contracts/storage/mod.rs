48743eb40b2c5a7394fe6be7219b49431aabe0cc
zielvna <zielvna@gmail.com>
2023-12-20 18:05:12 +0100

    change state to invariant config

--- src/contracts/storage/mod.rs
+++ src/contracts/storage/mod.rs
@@ -1,15 +1,15 @@
 pub mod fee_tier;
+pub mod invariant_config;
 pub mod pool;
 pub mod pool_key;
 pub mod position;
-pub mod state;
 pub mod tick;
 pub mod tickmap;
 
 pub use fee_tier::*;
+pub use invariant_config::*;
 pub use pool::*;
 pub use pool_key::*;
 pub use position::*;
-pub use state::*;
 pub use tick::*;
 pub use tickmap::*;
