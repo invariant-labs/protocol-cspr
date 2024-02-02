079a7426fc318dcb3e4d698dcb263b836d571567
Sniezka <dygons14@gmail.com>
2024-02-02 10:04:21 +0100

    Added local dependency to run nodes & deployed contract

--- sdk/src/client.ts
+++ sdk/src/client.ts
@@ -1,41 +1,202 @@
+import { BigNumber, type BigNumberish } from "@ethersproject/bignumber";
 import {
+  CLAccountHash,
   CLPublicKey,
   CLValueBuilder,
-  CasperClient,
+  CasperServiceByJsonRPC,
   Contracts,
+  DeployUtil,
   Keys,
   RuntimeArgs,
+  StoredValue,
+  encodeBase16,
 } from "casper-js-sdk";
 const { Contract } = Contracts;
 
-export class Client {
-  private casperClient: CasperClient;
+type Defined<Value> = Exclude<Value, null | undefined>;
+type Account = Defined<StoredValue["Account"]>;
 
-  public contractClient: Contracts.Contract;
+export class Client {
+  casperClient: CasperServiceByJsonRPC;
 
   constructor(public nodeAddress: string, public networkName: string) {
-    this.casperClient = new CasperClient(nodeAddress);
-    this.contractClient = new Contract(this.casperClient);
+    this.casperClient = new CasperServiceByJsonRPC(nodeAddress);
   }
 
-  public install(
-    contractName: string,
-    paymentAmount: string,
-    deploySender: CLPublicKey,
+  async getAccountInfo(account: CLPublicKey | CLAccountHash): Promise<Account> {
+    const accountHash =
+      account instanceof CLPublicKey
+        ? new CLAccountHash(account.toAccountHash())
+        : account;
+    const stateRootHash = await this.casperClient.getStateRootHash();
+    const accountInfo = await this.casperClient.getBlockState(
+      stateRootHash,
+      `account-hash-${encodeBase16(accountHash.value())}`,
+      []
+    );
+
+    if (!accountInfo.Account) {
+      throw new Error(`Account ${accountHash} not found`);
+    }
+
+    return accountInfo.Account;
+  }
+
+  async waitForDeploy(deploy: DeployUtil.Deploy | string) {
+    return this.casperClient.waitForDeploy(deploy);
+  }
+
+  private install(
     wasm: Uint8Array,
-    keys?: Keys.AsymmetricKey[]
+    args: RuntimeArgs,
+    paymentAmount: string,
+    sender: CLPublicKey,
+    chainName: string,
+    signingKeys: Keys.AsymmetricKey[] = []
   ) {
+    const deploy = DeployUtil.makeDeploy(
+      new DeployUtil.DeployParams(sender, chainName),
+      DeployUtil.ExecutableDeployItem.newModuleBytes(wasm, args),
+      DeployUtil.standardPayment(paymentAmount)
+    );
+
+    const signedDeploy = deploy.sign(signingKeys);
+
+    return signedDeploy;
+  }
+
+  addAssociatedKeys(
+    wasm: Uint8Array,
+    args: AddAssociatedKeysArgs,
+    paymentAmount: BigNumberish,
+    sender: CLPublicKey,
+    networkName?: string,
+    signingKeys?: Keys.AsymmetricKey[]
+  ): DeployUtil.Deploy {
     const runtimeArgs = RuntimeArgs.fromMap({
-      contract_name: CLValueBuilder.string(contractName),
+      keys: CLValueBuilder.list(
+        args.keys.map((key) =>
+          CLValueBuilder.tuple2([
+            key.account instanceof CLPublicKey
+              ? new CLAccountHash(key.account.toAccountHash())
+              : key.account,
+            CLValueBuilder.u8(key.weight),
+          ])
+        )
+      ),
     });
 
-    return this.contractClient.install(
+    return this.install(
       wasm,
       runtimeArgs,
-      paymentAmount,
-      deploySender,
-      this.networkName,
-      keys || []
+      BigNumber.from(paymentAmount).toString(),
+      sender,
+      networkName ?? this.networkName,
+      signingKeys
     );
   }
+
+  removeAssociatedKeys(
+    wasm: Uint8Array,
+    args: RemoveAssociatedKeysArgs,
+    paymentAmount: BigNumberish,
+    sender: CLPublicKey,
+    networkName?: string,
+    signingKeys?: Keys.AsymmetricKey[]
+  ): DeployUtil.Deploy {
+    const runtimeArgs = RuntimeArgs.fromMap({
+      keys: CLValueBuilder.list(
+        args.keys.map((key) =>
+          key instanceof CLPublicKey
+            ? new CLAccountHash(key.toAccountHash())
+            : key
+        )
+      ),
+    });
+
+    return this.install(
+      wasm,
+      runtimeArgs,
+      BigNumber.from(paymentAmount).toString(),
+      sender,
+      networkName ?? this.networkName,
+      signingKeys
+    );
+  }
+
+  updateAssociatedKeys(
+    wasm: Uint8Array,
+    args: UpdateAssociatedKeysArgs,
+    paymentAmount: BigNumberish,
+    sender: CLPublicKey,
+    networkName?: string,
+    signingKeys?: Keys.AsymmetricKey[]
+  ): DeployUtil.Deploy {
+    const runtimeArgs = RuntimeArgs.fromMap({
+      keys: CLValueBuilder.list(
+        args.keys.map((key) =>
+          CLValueBuilder.tuple2([
+            key.account instanceof CLPublicKey
+              ? new CLAccountHash(key.account.toAccountHash())
+              : key.account,
+            CLValueBuilder.u8(key.weight),
+          ])
+        )
+      ),
+    });
+
+    return this.install(
+      wasm,
+      runtimeArgs,
+      BigNumber.from(paymentAmount).toString(),
+      sender,
+      networkName ?? this.networkName,
+      signingKeys
+    );
+  }
+
+  updateThreshold(
+    wasm: Uint8Array,
+    args: UpdateThresholdArgs,
+    paymentAmount: BigNumberish,
+    sender: CLPublicKey,
+    networkName?: string,
+    signingKeys?: Keys.AsymmetricKey[]
+  ): DeployUtil.Deploy {
+    const runtimeArgs = RuntimeArgs.fromMap({
+      key_management_threshold: CLValueBuilder.u8(args.keyManagement),
+      deploy_threshold: CLValueBuilder.u8(args.deployment),
+    });
+
+    return this.install(
+      wasm,
+      runtimeArgs,
+      BigNumber.from(paymentAmount).toString(),
+      sender,
+      networkName ?? this.networkName,
+      signingKeys
+    );
+  }
+}
+
+export interface AssociatedKey {
+  account: CLPublicKey | CLAccountHash;
+  weight: number;
+}
+
+export interface AddAssociatedKeysArgs {
+  keys: AssociatedKey[];
+}
+
+export interface RemoveAssociatedKeysArgs {
+  keys: AssociatedKey["account"][];
+}
+
+export interface UpdateAssociatedKeysArgs {
+  keys: AssociatedKey[];
+}
+
+export interface UpdateThresholdArgs {
+  deployment: number;
+  keyManagement: number;
 }
--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,94 +1,50 @@
 import {
   CLValueBuilder,
   CasperClient,
+  CasperServiceByJsonRPC,
   Contracts,
+  Keys,
   RuntimeArgs,
 } from "casper-js-sdk";
+import { FUNDED_KEYS } from "casper-node-launcher-js";
 import { Client } from "./client";
-import { KEYS_ALGO, KEYS_PATH, NETWORK_NAME, NODE_ADDRESS } from "./consts";
-import { createAccountKeys, getWasm, parseAccountKeys, sleep } from "./utils";
+import { getWasm } from "./utils";
 
 const main = async () => {
   console.log("Init SDK!");
 
-  const accountAddress = createAccountKeys();
-  const parsedAddress = parseAccountKeys(KEYS_PATH, KEYS_ALGO);
-  const client = new Client(NODE_ADDRESS, NETWORK_NAME);
-  console.log(client);
+  const users = FUNDED_KEYS.map((k) => k.private).map((key) =>
+    Keys.getKeysFromHexPrivKey(key, Keys.SignatureAlgorithm.Ed25519)
+  );
+  const ALICE = users[0];
+  const BOB = users[1];
+  const CHARLIE = users[2];
 
-  await sleep(1000);
+  const NETWORK_URL = "http://127.0.0.1:7777/rpc";
+  const NETWORK_NAME = "casper-net-1";
 
-  //   const accountInfo = await getAccountInfo(
-  //     NODE_ADDRESS,
-  //     CLPublicKey.fromHex(accountAddress)
-  //   );
+  const client = new Client(NETWORK_URL, NETWORK_NAME);
+  const casperClient = new CasperServiceByJsonRPC(NETWORK_URL);
+  const accountInfo = await client.getAccountInfo(ALICE.publicKey);
+  const initialAssociatedKeys = accountInfo.associatedKeys;
 
-  const invariantWasm = getWasm("invariant");
-  const contractName = "Invariant";
+  const newCasperClient = new CasperClient(NETWORK_URL);
+  const contract = new Contracts.Contract(newCasperClient);
 
-  {
-    const casperClient = new CasperClient(NODE_ADDRESS + "/rpc");
-    const contract = new Contracts.Contract(casperClient);
-    const runtimeArguments = RuntimeArgs.fromMap({
-      fee: CLValueBuilder.string("100000000000"),
-    });
-    const deploy = contract.install(
-      invariantWasm,
-      runtimeArguments,
-      "10000000000",
-      parsedAddress.publicKey,
-      NETWORK_NAME,
-      [parsedAddress]
-    );
-    console.log(await casperClient.putDeploy(deploy));
-  }
+  const wasm = getWasm("invariant");
+  const runtimeArguments = RuntimeArgs.fromMap({
+    fee: CLValueBuilder.u256(100_000_000),
+  });
+  const deploy = contract.install(
+    wasm,
+    runtimeArguments,
+    "10000000000",
+    ALICE.publicKey,
+    "casper-net-1",
+    [ALICE]
+  );
+  console.log(await newCasperClient.putDeploy(deploy));
 
-  await sleep(1000);
-
-  {
-    const installDeploy = client.install(
-      contractName,
-      "100000000000",
-      // CLPublicKey.fromHex(accountAddress),
-      parsedAddress.publicKey,
-      invariantWasm,
-      [parsedAddress]
-    );
-
-    console.log(`... contract installation deploy`);
-    console.log(installDeploy);
-    const installDeployHash = await installDeploy.send(NODE_ADDRESS);
-    console.log(`... contract installation deployHash: ${installDeployHash}`);
-  }
-
-  //   await getDeploy(NODE_ADDRESS, installDeployHash);
-
-  //   const accountInfos = await getAccountInfo(
-  //     NODE_ADDRESS,
-  //     CONTRACT_OWNER.publicKey
-  //   );
-
-  //   console.log(`... Account Info: `);
-  //   console.log(JSON.stringify(accountInfos, null, 2));
-
-  //   const paymentsContractHash = await getAccountNamedKeyValue(
-  //     accountInfos,
-  //     `${contractName}_contract_hash`
-  //   );
-
-  //   const paymentsContractPackageHash = await getAccountNamedKeyValue(
-  //     accountInfos,
-  //     `${contractName}_contract_package_hash`
-  //   );
-
-  //   console.log(
-  //     `... Payments contract installed successfully. ${paymentsContractHash}`
-  //   );
-  //   console.log(`... Contract hash:         ${paymentsContractHash}`);
-  //   console.log(`... Contract package hash: ${paymentsContractPackageHash}`);
-  //   console.log(
-  //     `----------------------------------------------------------------------------------------------------`
-  //   );
   process.exit(0);
 };
 
