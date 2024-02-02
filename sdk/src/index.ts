3db9506455f08ef0569c1216b935858145fdee6d
Sniezka <dygons14@gmail.com>
2024-02-02 09:41:05 +0100

    Removed env

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,13 +1,12 @@
-import { CLPublicKey } from "casper-js-sdk";
+import {
+  CLValueBuilder,
+  CasperClient,
+  Contracts,
+  RuntimeArgs,
+} from "casper-js-sdk";
 import { Client } from "./client";
 import { KEYS_ALGO, KEYS_PATH, NETWORK_NAME, NODE_ADDRESS } from "./consts";
-import {
-  createAccountKeys,
-  getAccountInfo,
-  getWasm,
-  parseAccountKeys,
-  sleep,
-} from "./utils";
+import { createAccountKeys, getWasm, parseAccountKeys, sleep } from "./utils";
 
 const main = async () => {
   console.log("Init SDK!");
@@ -17,31 +16,50 @@ const main = async () => {
   const client = new Client(NODE_ADDRESS, NETWORK_NAME);
   console.log(client);
 
-  await sleep(10000);
+  await sleep(1000);
 
-  const accountInfo = await getAccountInfo(
-    NODE_ADDRESS,
-    CLPublicKey.fromHex(accountAddress)
-  );
-
-  console.log(accountInfo);
+  //   const accountInfo = await getAccountInfo(
+  //     NODE_ADDRESS,
+  //     CLPublicKey.fromHex(accountAddress)
+  //   );
 
   const invariantWasm = getWasm("invariant");
   const contractName = "Invariant";
 
-  const installDeploy = client.install(
-    contractName,
-    "10000000000000",
-    // CLPublicKey.fromHex(accountAddress),
-    parsedAddress.publicKey,
-    invariantWasm,
-    [parsedAddress]
-  );
+  {
+    const casperClient = new CasperClient(NODE_ADDRESS + "/rpc");
+    const contract = new Contracts.Contract(casperClient);
+    const runtimeArguments = RuntimeArgs.fromMap({
+      fee: CLValueBuilder.string("100000000000"),
+    });
+    const deploy = contract.install(
+      invariantWasm,
+      runtimeArguments,
+      "10000000000",
+      parsedAddress.publicKey,
+      NETWORK_NAME,
+      [parsedAddress]
+    );
+    console.log(await casperClient.putDeploy(deploy));
+  }
 
-  console.log(`... contract installation deploy`);
-  console.log(installDeploy);
-  const installDeployHash = await installDeploy.send(NODE_ADDRESS);
-  console.log(`... contract installation deployHash: ${installDeployHash}`);
+  await sleep(1000);
+
+  {
+    const installDeploy = client.install(
+      contractName,
+      "100000000000",
+      // CLPublicKey.fromHex(accountAddress),
+      parsedAddress.publicKey,
+      invariantWasm,
+      [parsedAddress]
+    );
+
+    console.log(`... contract installation deploy`);
+    console.log(installDeploy);
+    const installDeployHash = await installDeploy.send(NODE_ADDRESS);
+    console.log(`... contract installation deployHash: ${installDeployHash}`);
+  }
 
   //   await getDeploy(NODE_ADDRESS, installDeployHash);
 
