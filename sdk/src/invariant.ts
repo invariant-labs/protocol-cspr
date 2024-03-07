5b8a1840fafdde30910bb4a17113436b3af3237f
Sniezka <dygons14@gmail.com>
2024-03-07 09:09:00 +0100

    clear

--- sdk/src/invariant.ts
+++ sdk/src/invariant.ts
@@ -382,4 +382,31 @@ export class Invariant {
       }
     )
   }
+
+  async withdrawProtocolFee(
+    account: Keys.AsymmetricKey,
+    network: Network,
+    token0: string,
+    token1: string,
+    fee: bigint,
+    tickSpacing: bigint
+  ) {
+    const token0Key = new CLByteArray(decodeBase16(token0))
+    const token1Key = new CLByteArray(decodeBase16(token1))
+
+    return await sendTx(
+      this.contract,
+      this.service,
+      this.paymentAmount,
+      account,
+      network,
+      'withdraw_protocol_fee',
+      {
+        token_0: CLValueBuilder.key(token0Key),
+        token_1: CLValueBuilder.key(token1Key),
+        fee: CLValueBuilder.u128(BigNumber.from(fee)),
+        tick_spacing: CLValueBuilder.u32(integerSafeCast(tickSpacing))
+      }
+    )
+  }
 }
