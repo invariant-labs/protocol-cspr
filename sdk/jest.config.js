4c246298ef4533721ef934c856bd311beef976a9
Sniezka <dygons14@gmail.com>
2024-03-12 09:39:26 +0100

    Removed Jest

--- sdk/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testTimeout: 300000,
-  transform: {
-    '^.+\\.test.ts?$': 'ts-jest'
-  }
-}
